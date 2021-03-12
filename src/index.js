31c132cc0f9216091ff9c15a37344028c969798c
ShkrobNastya <60944202+ShkrobNastya@users.noreply.github.com>
2021-03-13 01:14:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  let map = new Map();
+
+  for (let j=0; j < bracketsConfig.length; j++) {
+    map.set(bracketsConfig[j][0], bracketsConfig[j][1]);
+  }
+
+  console.log(map);
+
+  let arr = str.slice('');
+  let st = [];
+
+  for (var i = 0; i < arr.length; i++) {
+      let ch = arr[i];
+      let ind = map.has(ch);
+      if (ind == false) {
+
+            if (!st.length) {
+              return false;
+            }
+            let lastBr = st.pop();
+            if (map.get(lastBr) != ch) {
+              return false;
+            }
+        } else { 
+            
+          if (ch == map.get(ch)) {
+            if (!st.length) {
+                st.push(ch);           
+            }  else if (st[st.length-1] !=ch) {
+                st.push(ch);
+            } else {
+                let lastBr = st.pop();
+                if (map.get(lastBr) != ch) {
+                    console.log("eee1");
+                  return false;
+                }
+            }      
+        } else {
+            st.push(ch);
+        }
+        }
+      }
+    if (st.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+};
